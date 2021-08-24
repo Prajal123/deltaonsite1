dc8b9af1b9005cc933925f2e775a6fb6c1867e29
Prajal123 <106120085@nitt.edu>
2021-08-24 21:43:38 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const buttons=document.querySelectorAll('button');
-var big=['tab','q','w','e','r','t','y','u','i','o','p','[',']','a','s','d','f','g','h','j','k'];
-var small=['z','x','c','v','b','n','m',',','.','/','control',' ','alt','shift'];
+var keypressed={};
+var big=['c','d','e','f','g','a','b'];
+var small=[ '1','2','3','4','5','6','7','8','9','0','-','=','[',']'];
+
 const whitekey=document.querySelectorAll('.big');
 const blackkey=document.querySelectorAll('.small');
 for(var i=0;i<buttons.length;i++){
@@ -17,10 +19,12 @@ for(var i=0;i<buttons.length;i++){
 }
 
 document.addEventListener("keydown",function(e){
+    keypressed[e.key]=true;
     if(e.repeat)return;
   const key=e.key;
   const whitekeyindex=big.indexOf(key);
   const blackkeyindex=small.indexOf(key);
+  if(keypressed['ArrowLeft']){
   if(whitekeyindex>-1){
       var a=whitekey[whitekeyindex];
     var notekey=document.getElementById(a.dataset.note);
@@ -29,7 +33,32 @@ document.addEventListener("keydown",function(e){
     notekey.addEventListener('ended',function(){
         a.classList.remove("active");
     })
+  
   }
+}else if(keypressed['ArrowRight']){
+    if(whitekeyindex>-1){
+        var a=whitekey[whitekeyindex+14];
+      var notekey=document.getElementById(a.dataset.note);
+      notekey.play();
+      a.classList.add("active");
+      notekey.addEventListener('ended',function(){
+          a.classList.remove("active");
+      })
+     
+      console.log(keypressed[e.key]);
+    }
+}else{
+    if(whitekeyindex>-1){
+        var a=whitekey[whitekeyindex+7];
+      var notekey=document.getElementById(a.dataset.note);
+      notekey.play();
+      a.classList.add("active");
+      notekey.addEventListener('ended',function(){
+          a.classList.remove("active");
+      })
+    }
+    
+}
   if(blackkeyindex>-1){
       var a=blackkey[blackkeyindex];
     var notekey=document.getElementById(a.dataset.note);
@@ -39,4 +68,9 @@ document.addEventListener("keydown",function(e){
         a.classList.remove("active");
     })
   }
+
+});
+
+document.addEventListener("keyup",function(e){
+    delete keypressed[e.key];
 })
