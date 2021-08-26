2085937a2518e4031ec2476439433e37ebcae924
Prajal123 <106120085@nitt.edu>
2021-08-26 14:24:48 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const buttons=document.querySelectorAll('button');
 var keypressed={};
+var volume=1;
 var big=['c','d','e','f','g','a','b'];
-var small=[ '1','2','3','4','5','6','7','8','9','0','-','=','[',']'];
+var small=['`', '1','2','3','4','5','6','7','8','9','0','-','=','[',']'];
 
 const whitekey=document.querySelectorAll('.big');
 const blackkey=document.querySelectorAll('.small');
-for(var i=0;i<buttons.length;i++){
+for(var i=0;i<buttons.length-1;i++){
    buttons[i].addEventListener("click",function(){
       var a=this;
     var notekey=document.getElementById(a.dataset.note);
+    notekey.volume=volume;
     notekey.currentTime=0;
     notekey.play();  
     a.classList.add("active");
-  notekey.addEventListener('ended',function(){
-      a.classList.remove("active");
-  })
+   setTimeout(function(){
+    a.classList.remove("active");
+   
+   }, 100);
    })
 }
 
@@ -74,3 +77,8 @@ document.addEventListener("keydown",function(e){
 document.addEventListener("keyup",function(e){
     delete keypressed[e.key];
 })
+
+function volume1(){
+  volume=prompt("Enter the volume in the range 0 to 1");
+  
+}
